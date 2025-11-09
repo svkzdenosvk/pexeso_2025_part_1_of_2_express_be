0dcda02a2da4088e664f3c46be1ec00e1930da70
https://github.com/svkzdenosvk <zdenko.snurik@gmail.com>
2025-11-09 05:35:44 +0100

    adjust type Number for port

--- src/index.ts
+++ src/index.ts
@@ -58,7 +58,7 @@ app.use((req, _res, next) => {
   next();
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server beží na https://tvoje-meno.onrender.com`);
   console.log(`Port: ${PORT}`);
